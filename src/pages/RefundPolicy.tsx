9545cd6a28671494bbc5a3a050c1ac910d974162
drybrite7-Laundry <drybrite7@gmail.com>
2025-09-09 16:34:32 +0530

    Update RefundPolicy.tsx

--- src/pages/RefundPolicy.tsx
+++ src/pages/RefundPolicy.tsx
@@ -1,5 +1,7 @@
 import Navigation from "@/components/Navigation";
-import Footer from "@/components/Footer";
+
+
+
 import { RefreshCw, CheckCircle, AlertCircle, Phone, Mail, MapPin } from "lucide-react";
 import { motion } from "framer-motion";
 
